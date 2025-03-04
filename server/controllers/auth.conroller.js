85014d42d947a3f8b556888e4a1b0d0209f7bf71
Kartikey Kakaria <kartikeykakaria07@gmail.com>
2025-03-04 11:15:38 +0530

    added middleware for error handling

--- server/controllers/auth.conroller.js
+++ server/controllers/auth.conroller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -11,7 +11,6 @@ export const signup = async (req, res) => {
       .status(201)
       .json({ message: "User created successfully", user: result });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Server Error", error });
+    next(error);
   }
 };
--- server/index.js
+++ server/index.js
@@ -11,6 +11,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message: err.message || "Something went wrong",
+    error: process.env.NODE_ENV === "development" && err,
+  });
+});
+
 app.listen(3000, () => {
   db();
   console.log("Listening at port 3000");
