66349f1659daa3b577cc8722e3959e6dfe9b7eaf
Kartikey Kakaria <kartikeykakaria07@gmail.com>
2025-04-06 11:13:48 +0530

    fixed export limit on backend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import OAuth from "../components/OAuth";
--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import Logging from "./utils/Logging.utils.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({
+  limit:"50mb"
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req, res, next) => {
